1d93d32cd07cdc1b8a907811ef335334b437260f
nocarryr <matt@nomadic-recording.com>
2011-11-25 21:09:15 +0000

    ***

--- subprocessing.py
+++ subprocessing.py
@@ -4,16 +4,66 @@ import multiprocessing.managers
 from osc_base import OSCBaseObject
 import Properties
     
-class SubProcessBase(OSCBaseObject):
+SUBPROCESSES = {}
+
+def get_subprocesses():
+    return SUBPROCESSES
+
+def BuildSubProcess(cls, name, parent=None, **kwargs):
+    assert name not in SUBPROCESSES, 'SubProcess %s already exists' % (name)
+    sp = SubProcess(cls=cls, name=name, SubProcessParent=parent, kwargs=kwargs)
+    SUBPROCESSES[sp.name] = sp
+    sp.start()
+    sp.obj_init.wait()
+    obj = sp.out_queue.get()
+    print name, obj
+    return obj
+    
+class SubProcess(multiprocessing.Process):
     def __init__(self, **kwargs):
-        super(SubProcessBase, self).__init__(**kwargs)
-        mkwargs = kwargs.copy()
-        mkwargs.setdefault('SubProcessParent', self)
-        mkwargs.setdefault('SubProcessName', getattr(self, 'SubProcessName', self.__class__.__name__))
-        self._subprocess_manager = Manager(**mkwargs)
-        self._subprocess_manager.start()
+        self.cls = kwargs.get('cls')
+        self._obj = None
+        pkwargs = {}
+        for key in ['name', 'args', 'kwargs']:
+            if key not in kwargs:
+                continue
+            pkwargs[key] = kwargs[key]
+        super(SubProcess, self).__init__(**pkwargs)
+        self.SubProcessParent = kwargs.get('SubProcessParent')
+        self.running = multiprocessing.Event()
+        self.obj_init = multiprocessing.Event()
+        self.in_queue = multiprocessing.Queue()
+        self.out_queue = multiprocessing.Queue()
+    @property
+    def obj(self):
+        return self._obj
+    def run(self):
+        in_queue = self.in_queue
+        running = self.running
+        running.set()
+        args = self._args
+        kwargs = self._kwargs
+        obj = self.cls(*args, **kwargs)
+        obj.SubProcess = self
+        self._obj = obj
+        self.send_item(obj)
+        self.obj_init.set()
+        while running.is_set():
+            item = in_queue.get()
+            if isinstance(item, QueueTerminator):
+                self.running.clear()
+            else:
+                self.process_item(item)
+    def stop(self):
+        self.in_queue.put(QueueTerminator())
+    def send_item(self, item):
+        self.out_queue.put(item)
+    def process_item(self, item):
+        pass
         
-        
+class QueueTerminator(object):
+    pass
+    
 class Manager(multiprocessing.managers.SyncManager):
     def __init__(self, **kwargs):
         self.SubProcessParent = kwargs.get('SubProcessParent')
