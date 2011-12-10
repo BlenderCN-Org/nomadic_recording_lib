0c6050839ee3d7531163c70110dceda5fe0a6866
nocarryr <matt@nomadic-recording.com>
2011-12-10 23:10:55 +0000

    ***

--- archive.py
+++ archive.py
@@ -19,6 +19,7 @@ import time
 import datetime
 import tarfile
 from StringIO import StringIO
+import csv
 
 import jsonpickle
 
@@ -44,9 +45,13 @@ class ArchiveMember(BaseObject):
         self.path = kwargs.get('path', '/')
         self.filename = kwargs.get('filename')
         self.id = kwargs.get('id', self.full_path)
+        self.serialize_method = kwargs.get('serialize_method', 'json')
+        self._do_serialize = getattr(self, '_serialize_' + self.serialize_method)
+        self._do_deserialize = getattr(self, '_deserialize_' + self.serialize_method)
         self.serialize_obj = kwargs.get('serialize_obj', {})
         self.serialize_kwargs = kwargs.get('serialize_kwargs', {})
         self.deserialize_kwargs = kwargs.get('deserialize_kwargs', {})
+        self.csv_columns = kwargs.get('csv_columns')
         self.datetime_format = kwargs.get('datetime_format', '%Y%m%d-%H:%M:%S')
         self.json_preset = kwargs.get('json_preset', 'pretty')
         self.file_created = None
@@ -62,7 +67,8 @@ class ArchiveMember(BaseObject):
             self.file_modified = self.file_created
         if self.file_modified is None:
             self.file_modified = build_datetime_string(self.datetime_format)
-        file = StringIO(self._serialize())
+        file = StringIO()
+        self._do_serialize(file)
         file.seek(0)
         tinf = tarfile.TarInfo(self.full_path)
         tinf.size = len(file.buf)
@@ -71,15 +77,18 @@ class ArchiveMember(BaseObject):
         return file, tinf
         
     def load(self, tar):
-        js = ''
         try:
             tinf = tar.getmember(self.full_path)
             file = tar.extractfile(tinf)
         except KeyError:
             return
+        self._do_deserialize(file)
+        file.close()
+        
+    def _deserialize_json(self, file):
+        js = ''
         for line in file:
             js += line
-        file.close()
         d = jsonpickle.decode(js)
         self._load_saved_attr(d)
         for key, objdict in d['serialize_obj'].iteritems():
@@ -89,16 +98,40 @@ class ArchiveMember(BaseObject):
             dskwargs = self.deserialize_kwargs.get(key, {})
             obj._load_saved_attr(objdict, **dskwargs)
         
-    def _serialize(self):
+    def _serialize_json(self, file):
         d = self._get_saved_attr()
         d['serialize_obj'] = {}
         for key, obj in self.serialize_obj.iteritems():
             skwargs = self.serialize_kwargs.get(key, {})
             d['serialize_obj'][key] = obj._get_saved_attr(**skwargs)
         jsonpickle.set_encoder_options('simplejson', **json_presets[self.json_preset])
-        return jsonpickle.encode(d, unpicklable=False)
+        s = jsonpickle.encode(d, unpicklable=False)
+        file.write(s)
         
-
+    def _deserialize_csv(self, file):
+        pass
+        
+    def _serialize_csv(self, file):
+        keys = self.csv_columns
+        serialize_obj = self.serialize_obj
+        if callable(serialize_obj):
+            serialize_obj = serialize_obj()
+        if keys is None:
+            keys = serialize_obj[0].keys()
+        headers = self.serialize_kwargs.get('write_headers', True)
+        indexed = self.serialize_kwargs.get('add_index', True)
+        if indexed:
+            keys.insert(0, 'index')
+        writer = csv.DictWriter(file, fieldnames=keys)
+        
+        if headers:
+            writer.writerow(dict(zip(keys, [key.title() for key in keys])))
+        for i, d in enumerate(serialize_obj):
+            d = d.copy()
+            if indexed:
+                d.setdefault('index', i)
+            writer.writerow(d)
+        
 class Archive(BaseObject):
     _ChildGroups = {'members':{'child_class':ArchiveMember}}
     def __init__(self, **kwargs):
