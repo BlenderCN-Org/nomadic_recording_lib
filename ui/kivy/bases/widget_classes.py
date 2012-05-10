e59ea2689aa7e0b70ed137e61c72a1ee93b8ddfb
nocarryr <matt@nomadic-recording.com>
2012-05-10 16:01:28 +0000

    ***

--- ui/kivy/bases/widget_classes.py
+++ ui/kivy/bases/widget_classes.py
@@ -7,7 +7,7 @@ class Widgets(object):
 
 iter = pkgutil.iter_modules(uix.__path__)
 for imp, name, ispkg in iter:
-    if name not in ['__init__', 'svg']:
+    if name not in ['__init__', 'svg', 'rst']:
         #print 'importing ', name, ', path:', '.'.join([uix.__name__, name])
         mod = __import__('.'.join([uix.__name__, name]), fromlist='dummy')
         for key, val in mod.__dict__.iteritems():
